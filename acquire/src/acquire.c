483d66b3c8e49615e519610c2152ded1b73b10ad
Matt Rounds <rounds.m2@gmail.com>
2017-11-28 06:12:27 +0000

    add GPL license info to source files

--- acquire/src/acquire.c
+++ acquire/src/acquire.c
@@ -3,8 +3,20 @@
 // in the channel array. Channel details are stored as JSON for reading by
 // ../serve/serve.js, which updates the channel values on a live webpage.
 //
-// Created by: Matt Rounds
-// Last updated: Nov 25, 2017
+// Copyright (C) 2017 Matt Rounds
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <endian.h>
 #include <errno.h>
@@ -21,6 +33,7 @@
 
 #include "acquire.h"
 
+#define M400_ID 0x05F0
 #define BITS_PER_BYTE 8
 #define MAX_CH_STR 32
 #define MS100_TO_NS 100000000
